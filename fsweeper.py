064cdd8353a41bc14757dbe0594353ed3f532184
Łukasz Boczkaja <lboczkaja@unity-t.pl>
2022-01-26 06:42:39 +0100

    Added argument handling and usage function

--- fsweeper.py
+++ fsweeper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Desc: file sweeper - organize files by their extension in given directory 
+# Desc: file sweeper - organize files by their extension in given directory
 import os
 from os.path import join
 import sys
@@ -8,9 +8,17 @@ import sys
 import shutil
 
 
-workdir = "./messy_dir"
 
-#known_extensions = ('mp3', 'avi', 'pdf', 'torrent', 'iso', 'zip', 'gz')
+def usage():
+    print("Usage: {} DIR".format(sys.argv[0]))
+    sys.exit(1)
+
+if len(sys.argv) != 2 or not os.path.isdir(sys.argv[1]):
+    usage()
+
+
+workdir = sys.argv[1]
+
 archive_formats = ['zip', 'gz', 'tar', '7z', 'lzma']
 audio_formats = ['mp3', 'ogg', 'mp4a', 'wav']
 video_formats = ['mp4', 'avi', 'mkv', 'webm', 'mpg', 'mpeg']
@@ -18,7 +26,7 @@ image_formats = ['gif', 'jpeg', 'jpg', 'bmp', 'png']
 text_formats = ['pdf', 'doc', 'docx', 'xls', 'odf', 'txt', 'xlsx']
 
 known_extensions = { 'archive': archive_formats, 'audio': audio_formats, 'video': video_formats, 'image': image_formats, 'torrent': ['torrent'], 'text': text_formats }
-#known_extensions = ('mp3', 'avi', 'pdf', 'torrent', 'iso', 'zip', 'gz'}
+
 workdir_entries = os.listdir(workdir)
 files = []
 
@@ -26,7 +34,7 @@ files = []
 for workdir_entry in workdir_entries:
     if os.path.isfile(join(os.getcwd(), workdir, workdir_entry)):
       files.append(workdir_entry)
-    
+
 
 for category, extensions in known_extensions.items():
     for file in files:
